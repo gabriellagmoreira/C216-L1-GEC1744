58dbf0338841931c376931adbfbc7e221054e392
Gabriellagmoreira <gabigmoreiraa@gmail.com>
2024-03-08 16:48:29 -0300

    Pratica 0

---
+++ pratica-0/src/index.js
@@ -0,0 +1,9 @@
+// Exemplo de uso prático
+let nome = "Gabriella Gonçalves Moreira- 1744";
+function saudacaoPersonalizada(nome) {
+    console.log("Olá, " + nome + "!");
+}
+saudacaoPersonalizada(nome);
+
+let modulo = require('./module.js');
+modulo.saudacao();
---
+++ pratica-0/src/module.js
@@ -0,0 +1,7 @@
+// arquivo modulo.js
+module.exports = {
+    mensagem: 'Testando modulo: Gabriella Moreira-1744 !',
+    saudacao: function() {
+      console.log(this.mensagem);
+    }
+  };
